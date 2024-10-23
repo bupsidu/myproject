aaf8c5de17341698d59265e7228313e5454b75c1
bubu <an.barreras@duocuc.cl>
2024-10-23 19:54:53 -0300

    terminando la api

--- rest_app/models.py
+++ rest_app/models.py
@@ -1,10 +1,8 @@
-from django.db import models
-
-# Create your models here.
-
+# rest_app/models.py
 from django.db import models
 
 class User(models.Model):
+    username = models.CharField(max_length=100, unique=True) 
     name = models.CharField(max_length=100)
     lastname = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
--- rest_app/views.py
+++ rest_app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
 # rest_app/views.py
-from rest_framework import viewsets
+from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from .models import User
 from .serializers import UserSerializer
 
@@ -8,3 +9,22 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
